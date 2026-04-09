96132dd4bdbcf4821bd69b59c27e026c747e5b7d
Easin2023 <mdeasinarafat016456@gmail.com>
2026-04-09 13:53:27 +0600

    added modulus operator

--- arithmetie_operators.c
+++ arithmetie_operators.c
@@ -13,5 +13,8 @@ int main()
     printf("The multiplication of %d and %d is: %d\n", a, b, multiplication);
     printf("The division of %d and %d is: %d\n", a, b, division);
     printf("The modulus of %d and %d is: %d\n", a, b, modulus);
+    /**
+     * This is a mod operator example in c. the mod operator is used to find the remainder of a division operation. in this expample, we are finding the modulus of 10 and 5, which is 0 because 10 is divisible by 5. if we were to find the modulus of 11 and 5, it would be 1 because 11 divided by 5 leaves a remainder of 1.
+     * */ 
     return 0;
 }
